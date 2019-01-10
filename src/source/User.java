a4df8fc9a62041ec58004431b85f0cdf74f0b611
autumn2014 <283139246@qq.com>
2019-01-10 22:04:31 +0800

    User属性函数

--- src/source/User.java
+++ src/source/User.java
@@ -2,4 +2,28 @@ package source;
 
 public class User {
     String name,pwd,addr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
 }
